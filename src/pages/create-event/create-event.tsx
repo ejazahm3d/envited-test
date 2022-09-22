4643ac479e99e12e31851c5ad19875439edef94e
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2022-09-22 14:18:55 +0500

    feat: half done event page

--- src/pages/create-event/create-event.tsx
+++ src/pages/create-event/create-event.tsx
@@ -8,12 +8,10 @@ import {
 } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { TimePicker } from "@mui/x-date-pickers/TimePicker";
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+
 import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
 import { FormEvent, useState } from "react";
-import dayjs, { Dayjs } from "dayjs";
+import { Dayjs } from "dayjs";
 import { useRouter } from "next/router";
 
 export const CreateEventPage = () => {
--- src/pages/event/event.tsx
+++ src/pages/event/event.tsx
@@ -1,3 +1,54 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
+
 export const EventPage = () => {
-  return <>Event Page</>;
+  return (
+    <>
+      <Box
+        width="100%"
+        component="img"
+        src="/birthday-cake.png"
+        alt="event photo"
+      />
+
+      <Container>
+        <Stack>
+          <Stack sx={{ mb: "1rem" }}>
+            <Typography
+              fontSize="28px"
+              sx={{
+                fontWeight: "bold",
+                color: "primary.darker",
+              }}
+            >
+              Birthday Bash
+            </Typography>
+            <Typography
+              fontSize="14px"
+              sx={{
+                color: "#828282",
+              }}
+            >
+              Hosted By{" "}
+              <Box
+                component="span"
+                sx={{
+                  fontWeight: "bold",
+                }}
+              >
+                Elysia
+              </Box>
+            </Typography>
+          </Stack>
+
+          <Stack flexDirection="row">
+            <Box sx={{}}>Icon</Box>
+            <Stack>
+              <Typography>18 August 6:00PM</Typography>
+              <Typography>to 19 August 1:00PM UTC +10</Typography>
+            </Stack>
+          </Stack>
+        </Stack>
+      </Container>
+    </>
+  );
 };
